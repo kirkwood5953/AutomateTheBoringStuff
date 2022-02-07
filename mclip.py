2168364232735cdfea8723ca131984171076d089
kirkwood5953 <kylekirkwood5953@gmail.com>
2022-02-06 16:08:31 -0800

    Create mclip.py

---
+++ mclip.py
@@ -0,0 +1,6 @@
+# Write your code here :-)
+#! python 3
+# mclip.py - A multi-clipboard program.
+
+TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
+        'busy': """Sorry, can we doo thtis later this week or next week?"""
